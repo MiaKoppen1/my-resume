9946e34d1235a0c5182b0cec86a89c0303df29d5
miakoppen1 <mkoppen2@gmail.com>
2025-08-07 18:10:56 -0500

    v1

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+export const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
