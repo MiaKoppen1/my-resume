9628cff6e4614f430a07aa4fffa632c4fdcaf596
miakoppen1 <mkoppen2@gmail.com>
2025-08-07 19:52:12 -0500

    v4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { NavBar } from './components/NavBar';
+import { Banner } from "./components/Banner";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+import { NavBar } from "./components/NavBar";
+import { Projects } from "./components/Projects";
+import { Skills } from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
-     <NavBar />
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
